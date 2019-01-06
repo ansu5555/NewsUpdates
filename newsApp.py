afc36f9b9f01b9676df8e4b4a508a46844ff9006
Ansuman <ansuman5555@gmail.com>
2019-01-07 00:13:28 +0530

    Changed templates

--- newsApp.py
+++ newsApp.py
@@ -1,3 +1,7 @@
+import os
+import json
+
+import requests
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +9,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('news.html')
+    api_key = os.environ.get('NEWSAPI_KEY')
+    resp = requests.get(f'https://newsapi.org/v2/top-headlines?country=in&apiKey={api_key}')
+    resp_dict = json.loads(resp.text)
+    articles = resp_dict['articles']
+    return render_template('news.html', articles=articles)
 
 
 if __name__ == '__main__':
