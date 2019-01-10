952d012bc2855fe6150222e4f1f170bfd8f6e725
Ansuman <ansuman5555@gmail.com>
2019-01-11 00:25:08 +0530

    Added database migration files

--- newsapp/__init__.py
+++ newsapp/__init__.py
@@ -1,14 +1,13 @@
 import os
 
 from flask import Flask
+from flask_apscheduler import APScheduler
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
-from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '781acf22041dc71f67c55eafe98584e5'
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URI')
-#qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 scheduler = APScheduler()
@@ -16,4 +15,4 @@ scheduler.api_enabled = True
 scheduler.init_app(app)
 scheduler.start()
 
-from newsapp import routes, util    # noqa
+from newsapp import routes, util  # noqa
